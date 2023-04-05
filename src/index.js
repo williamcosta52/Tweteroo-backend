60ea7bad484e3a1342acdc5c16124b4d3fc3032a
William <wiliancosta51@hotmail.com>
2023-04-05 15:38:46 -0300

    Requisito: get /tweets

--- src/index.js
+++ src/index.js
@@ -6,19 +6,36 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-const usuario = [];
+const users = [];
 const tweets = [];
 
 app.post("/sign-up", (req, res) => {
 	const user = req.body;
-	usuario.push(user);
+	users.push(user);
+	res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+	const tweet = req.body;
+	tweets.push(tweet);
 	res.send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-	const tweet = req.body;
-	tweets.push(tweet);
-	res.send("OK");
+	const lastTen = [];
+	if (tweets.length > 0) {
+		for (let i = tweets.length - 1; i > tweets.length - 11 && i >= 0; i--) {
+			const name = tweets[i].username;
+			const user = users.find((i) => i.username === name);
+			const body = {
+				username: name,
+				avatar: user.avatar,
+				tweet: tweets[i].tweet,
+			};
+			lastTen.push(body);
+		}
+	}
+	res.send(lastTen);
 });
 
 app.listen(5000);
